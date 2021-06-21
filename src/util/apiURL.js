07516d5e28f6ab591a275d826e7b20e22c226c6a
Kobe <tahiriadam@gmail.com>
2021-06-21 19:21:36 -0400

    fixed api func

--- src/util/apiURL.js
+++ src/util/apiURL.js
@@ -1,12 +1,9 @@
-
 export const apiURL = () => {
-    if (
-        process.env.NODE_ENV === "development" ||
-        process.env.NODE_ENV === "test"
-      ) {
-        return "http://localhost:3024";
-      } else {
-        return "https://mighty-springs-93765.herokuapp.com/";
-      }
-}
-
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    return "http://localhost:3024";
+  }
+  return "https://mighty-springs-93765.herokuapp.com";
+};
